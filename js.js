24f953cc2cdf60ca2939f4ab9f5f22b4bedfc00f
Janani.M <114896359+Jananimdev@users.noreply.github.com>
2023-08-15 12:16:14 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,104 @@
+var myNodelist = document.getElementsByTagName("LI");              
+var i;              
+for (i = 0; i < myNodelist.length; i++) {              
+  var span = document.createElement("SPAN");              
+  var txt = document.createTextNode("\u00D7");              
+  span.className = "close";              
+  span.appendChild(txt);              
+  myNodelist[i].appendChild(span);              
+}              
+                  
+var close = document.getElementsByClassName("close");              
+var i;              
+for (i = 0; i < close.length; i++) {              
+  close[i].onclick = function() {              
+    var div = this.parentElement;              
+    div.style.display = "none";              
+  }              
+}              
+                          
+var list = document.querySelector('ul');              
+list.addEventListener('click', function(ev) {              
+  if (ev.target.tagName === 'LI') {              
+    ev.target.classList.toggle('checked');              
+  }              
+}, false);              
+                          
+function newElement() {              
+  var li = document.createElement("li");              
+  var inputValue = document.getElementById("myInput").value;              
+  var t = document.createTextNode(inputValue);              
+  li.appendChild(t);              
+  if (inputValue === '') {              
+    alert("You must write something!");              
+  } else {              
+    document.getElementById("myUL").appendChild(li);              
+  }              
+  document.getElementById("myInput").value = "";              
+              
+  var span = document.createElement("SPAN");              
+  var txt = document.createTextNode("\u00D7");              
+  span.className = "close";              
+  span.appendChild(txt);              
+  li.appendChild(span);              
+              
+  for (i = 0; i < close.length; i++) {              
+    close[i].onclick = function() {              
+      var div = this.parentElement;              
+      div.style.display = "none";              
+    }              
+  }              
+}      
+function newElement() {
+  var inputValue = document.getElementById("myInput").value;
+  if (inputValue === '') {
+    alert("You must write something!");
+    return;
+  }
+
+  var li = document.createElement("li");
+  var taskSpan = document.createElement("span");
+  taskSpan.innerText = inputValue;
+
+  var editBtn = document.createElement("span");
+  editBtn.innerText = "Edit";
+  editBtn.className = "editBtn";
+  editBtn.onclick = function() {
+    editTask(taskSpan);
+  };
+
+  var deleteBtn = document.createElement("span");
+  deleteBtn.innerText = "Delete";
+  deleteBtn.className = "deleteBtn";
+  deleteBtn.onclick = function() {
+    deleteTask(li);
+  };
+
+  li.appendChild(taskSpan);
+  li.appendChild(editBtn);
+  li.appendChild(deleteBtn);
+
+  document.getElementById("myUL").appendChild(li);
+  document.getElementById("myInput").value = "";
+}
+
+function deleteTask(taskElement) {
+  var listItem = taskElement.parentElement;
+  listItem.remove();
+}
+
+function editTask(taskElement) {
+  var newTask = prompt("Edit task:", taskElement.innerText);
+  if (newTask !== null && newTask.trim() !== '') {
+    taskElement.innerText = newTask;
+  }
+}
+  function logout() {
+    
+   // localStorage.removeItem('authToken'); 
+    
+    
+    window.location.href = 'index.html'; 
+  }
+
+
